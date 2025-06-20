83d302c3de54d9b4e131e437aa39ad73b3cd5461
YoelEsparza <2237000181@utna.edu.mx>
2025-06-19 19:35:35 -0600

    Se realiza el API REST

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-const express = require('express');
-const app = express()
-const PORT = 3000;
+const app = require('./app/app')
+const config = require('./app/config/configuracion')
 
-app.listen(PORT,()=>{
+app.listen(config.PORT,()=>{
     console.log(`Aplicación corriendo en puerto $
         {PORT}`);
 })
