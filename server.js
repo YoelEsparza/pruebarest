c06578e4d1196d36ef3d5f45c525354951dcb832
YoelEsparza <2237000181@utna.edu.mx>
2025-06-26 19:35:44 -0600

    Se realiza la conexión desde Mongoose a MongoDB

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const app = require('./app/app')
 const config = require('./app/config/configuracion')
+const conexion = require('./app/config/conexion')
+
+conexion.connect()
 
 app.listen(config.PORT,()=>{
-    console.log(`Aplicación corriendo en puerto $
-        {PORT}`);
+    console.log(`Aplicación corriendo en puerto ${config.PORT}`);
 })
